0a3eff23fde1041f08a6ff27e4b7358a020b22e0
OLUWAGBENGA EMMANUEL ARIYO <ariyooe2000@gmail.com>
2025-02-03 15:43:56 +0000

    Changed the name

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyFirstAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "EMMANUEL",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
